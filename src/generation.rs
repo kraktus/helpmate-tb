bdebd2aae0eae0799a29ccc942964e53d4d34e62
kraktus <kraktus@users.noreply.github.com>
2021-11-30 13:53:47 +0100

    switch to pattern matching, untested

--- src/generation.rs
+++ src/generation.rs
@@ -1,8 +1,7 @@
 use crate::TbSetup;
 use retroboard::RetroBoard;
 use shakmaty::{
-    CastlingMode::Standard, Color::Black, Color::White, FromSetup, Piece, Position,
-    Setup, Square,
+    CastlingMode::Standard, Color::Black, Color::White, FromSetup, Piece, Position, Setup, Square,
 };
 use std::collections::{HashMap, VecDeque};
 use std::ops::{Add, Not};
@@ -46,33 +45,35 @@ pub struct Generator {
 }
 
 impl Generator {
-    pub fn generate_positions(&mut self, piece_vec: Vec<Piece>, setup: TbSetup) {
-        if let Some(piece) = piece_vec.pop() {
-        	println!("{:?}, setup: {:?}", piece, &setup);
-            let range = if piece == White.king() { 0..10 } else { 0..64 };
-            for sq in range.map(Square::new) {
-                
-                //println!("before {:?}", &setup);
-                if setup.board.piece_at(sq).is_none() {
-                	let mut new_setup = setup.clone();
-                	new_setup.board.set_piece_at(sq, piece);
-                	self.generate_positions(piece_vec, new_setup);
+    pub fn generate_positions(&mut self, piece_vec: &[Piece], setup: TbSetup) {
+        match piece_vec {
+            [piece, tail @ ..] => {
+                println!("{:?}, setup: {:?}", piece, &setup);
+                let range = if *piece == White.king() { 0..10 } else { 0..64 };
+                for sq in range.map(Square::new) {
+                    //println!("before {:?}", &setup);
+                    if setup.board.piece_at(sq).is_none() {
+                        let mut new_setup = setup.clone();
+                        new_setup.board.set_piece_at(sq, *piece);
+                        self.generate_positions(tail, new_setup);
+                    }
+                    //println!("after {:?}", &new_setup);
                 }
-                //println!("after {:?}", &new_setup);
             }
-        } else {
-            // setup is complete, check if valid
-            for color in [Black, White] {
-                let mut valid_setup = setup.clone();
-                valid_setup.turn = Some(color);
-                if let Ok(chess) = dbg!{&valid_setup}.to_chess_with_illegal_checks() {
-                    // if chess is valid then rboard should be too
-                    let rboard = RetroBoard::from_setup(&valid_setup, Standard).unwrap();
-                    if chess.is_checkmate() {
-                        self.all_pos.insert(rboard.clone(), Outcome::Lose(0));
-                        self.pos_to_process.push_back(rboard);
-                    } else {
-                        self.all_pos.insert(rboard.clone(), Outcome::Draw);
+            [] => {
+                // setup is complete, check if valid
+                for color in [Black, White] {
+                    let mut valid_setup = setup.clone();
+                    valid_setup.turn = Some(color);
+                    if let Ok(chess) = dbg! {&valid_setup}.to_chess_with_illegal_checks() {
+                        // if chess is valid then rboard should be too
+                        let rboard = RetroBoard::from_setup(&valid_setup, Standard).unwrap();
+                        if chess.is_checkmate() {
+                            self.all_pos.insert(rboard.clone(), Outcome::Lose(0));
+                            self.pos_to_process.push_back(rboard);
+                        } else {
+                            self.all_pos.insert(rboard.clone(), Outcome::Draw);
+                        }
                     }
                 }
             }
@@ -81,17 +82,17 @@ impl Generator {
 
     pub fn process_positions(&mut self) {
         if let Some(rboard) = self.pos_to_process.pop_front() {
-        	let out = *self.all_pos.get(&rboard).unwrap();
+            let out = *self.all_pos.get(&rboard).unwrap();
             for m in rboard.legal_unmoves() {
                 let mut rboard_after_unmove = rboard.clone();
                 rboard_after_unmove.push(&m);
                 if self.all_pos.get(&rboard_after_unmove).is_none() {
-                	println!("pos not found, illegal? {:?}", rboard_after_unmove)
+                    println!("pos not found, illegal? {:?}", rboard_after_unmove)
                 }
                 self.pos_to_process.push_back(rboard_after_unmove.clone()); // TODO currently the same pos can be scanned multiple times
                 self.all_pos.insert(rboard_after_unmove, (!out) + 1); //relative to player to move
             }
-            return self.process_positions()
+            return self.process_positions();
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod generation;
 mod setup;
 
-pub use setup::TbSetup;
 use generation::Generator;
+pub use setup::TbSetup;
 
-use shakmaty::{Color::{Black, White}};
+use shakmaty::Color::{Black, White};
 
 fn main() {
     println!("Hello, world!");
     let mut gen = Generator::default();
-    let mut vec_pieces = vec![Black.king(),White.queen(), White.king()];
+    let mut vec_pieces = vec![Black.king(), White.queen(), White.king()];
     let setup = TbSetup::default();
     println!("gen before {:?}", gen);
     gen.generate_positions(&mut vec_pieces, setup);
